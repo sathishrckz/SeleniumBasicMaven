59e88e5920f06c9f8b2b2dc7cc013f43999bde3c
sathishkumar <sathishrckz@gmail.com>
2022-02-09 12:13:20 +0530

    initial commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestClass {
+    public static void main(String[] args) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        // Navigate to the demoqa website
+        driver.get("https://www.amazon.com/");
+
+        driver.quit();
+
+    }
+}
